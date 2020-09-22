05d06699244703526df800de72743d190618b481
Julian Botero <julian.botero@ceiba.com.co>
2020-09-22 18:39:43 -0500

    [gral]: Se crea el backend de FutbolSiete

---
+++ Backend/FutbolSiete/src/main/java/com/ceiba/adn/FutbolSiete/FutbolSieteApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.adn.FutbolSiete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolSieteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolSieteApplication.class, args);
+	}
+
+}
