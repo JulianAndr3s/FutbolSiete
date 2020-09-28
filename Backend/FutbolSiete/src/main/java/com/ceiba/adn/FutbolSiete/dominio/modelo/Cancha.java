2fbca2981257bb0ef0a047d1547ff6601aa11e0a
Julian Botero <julian.botero@ceiba.com.co>
2020-09-28 10:46:23 -0500

    [backend]: Se realiza validaciones para el dominio Cancha

--- Backend/FutbolSiete/src/main/java/com/ceiba/adn/FutbolSiete/dominio/modelo/Cancha.java
+++ Backend/FutbolSiete/src/main/java/com/ceiba/adn/FutbolSiete/dominio/modelo/Cancha.java
@@ -1,6 +1,12 @@
 package com.ceiba.adn.FutbolSiete.dominio.modelo;
 
+import com.ceiba.adn.FutbolSiete.dominio.validaciones.ValidarArgumento;
+
 public class Cancha {
+
+    private static final String SE_DEBE_INGRESAR_LA_UBICACION_DE_LA_CANCHA = "Se debe ingresar la ubicacion de la cancha";
+    private static final String SE_DEBE_INGRESAR_CANTIDAD_DE_JUGADORES = "Se debe ingresar la cantidad de jugadores";
+    private static final String SE_DEBE_INGRESAR_EL_VALOR_DE_LA_CANCHA = "Se debe ingresar el valor de la cancha";
 
     private Long id;
     private String ubicacion;
@@ -8,6 +14,13 @@ public class Cancha {
     private Long valorCancha;
 
     public Cancha(Long id, String ubicacion, Long cantidadJugadores, Long valorCancha) {
+
+        ValidarArgumento.validarObligatorio(ubicacion, SE_DEBE_INGRESAR_LA_UBICACION_DE_LA_CANCHA);
+        ValidarArgumento.validarNoVacio(ubicacion, SE_DEBE_INGRESAR_LA_UBICACION_DE_LA_CANCHA);
+
+        ValidarArgumento.validarObligatorio(cantidadJugadores, SE_DEBE_INGRESAR_CANTIDAD_DE_JUGADORES);
+        ValidarArgumento.validarObligatorio(valorCancha, SE_DEBE_INGRESAR_EL_VALOR_DE_LA_CANCHA);
+
         this.id = id;
         this.ubicacion = ubicacion;
         this.cantidadJugadores = cantidadJugadores;
