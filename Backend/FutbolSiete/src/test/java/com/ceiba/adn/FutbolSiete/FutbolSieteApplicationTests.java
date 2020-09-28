29a715198af7314a40ac8f10050f1140238e8d57
Julian Botero <julian.botero@ceiba.com.co>
2020-09-28 09:09:43 -0500

    [gral]: Se modifican algunas lineas para buscar la covertura

--- Backend/FutbolSiete/src/test/java/com/ceiba/adn/FutbolSiete/FutbolSieteApplicationTests.java
+++ Backend/FutbolSiete/src/test/java/com/ceiba/adn/FutbolSiete/FutbolSieteApplicationTests.java
@@ -1,13 +1,8 @@
 package com.ceiba.adn.FutbolSiete;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FutbolSieteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
