8b4480036ce345cd8ba417240260f58dff2a188e
Julian Botero <julian.botero@ceiba.com.co>
2020-10-06 22:20:37 -0500

    [backend]: Se maneja el estado de los errores a enviar

--- Backend/FutbolSiete/src/main/java/com/ceiba/adn/futbolsiete/dominio/excepcion/ExcepcionValorVacio.java
+++ Backend/FutbolSiete/src/main/java/com/ceiba/adn/futbolsiete/dominio/excepcion/ExcepcionValorVacio.java
@@ -1,5 +1,9 @@
 package com.ceiba.adn.futbolsiete.dominio.excepcion;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ExcepcionValorVacio extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
